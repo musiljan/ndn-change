the new code with the latest library - returns wrong results

FFnetwork/ffnetwork.py
@@ -4,14 +4,8 @@ from __future__ import print_function
 from __future__ import division
 
 import tensorflow as tf
-import numpy as np
 from .layer import *
-#from .layer import ConvLayer
-#from .layer import SepLayer
-#from .layer import ConvSepLayer
-#from .layer import AddLayer
-#from .layer import SpikeHistoryLayer
-#from .layer import BiConvLayer
+
 
 class FFNetwork(object):
     """Implementation of simple fully-connected feed-forward neural network. 
@@ -46,38 +40,45 @@ class FFNetwork(object):
                 [num_lags, num_x_pix, num_y_pix]. If the input does not have 
                 spatial or temporal structure, this should be 
                 [1, num_inputs, 1]
-            params_dict (dict): contains parameters about details of FFnetwork
-            params_dict['layer_sizes'] (list of ints): list of layer sizes, 
-                including input and output. All arguments (input size) can be 
-                up to a 3-dimensional list. 
-                REQUIRED (NO DEFAULT)
-            params_dict['num_inh'] (int or list of ints): denotes number of 
-                inhibitory units in each layer. This specifies the output of 
-                that number of units multiplied by -1
-                DEFAULT = 0 (and having any single value will be used for all 
-                layers)
-            params_dict['activation_funcs'] (str or list of strs, optional): 
-                pointwise function for each layer; replicated if a single 
-                element. 
-                DEFAULT = 'relu'. See Layer class for other options.
-            params_dict['pos_constraints'] (bool or list of bools, optional): 
-                constrains all weights to be positive
-                DEFAULTS = False.
-            params_dict['reg_initializer'] (list): a list of dictionaries, one 
-                for each layer. Within the dictionary, reg_type/vals as 
-                key-value pairs.
-                DEFAULT = None
-            params_dict['weights_initializer'] (str or list of strs, optional): 
-                initializer for the weights in each layer; replicated if a 
-                single element.
-                DEFAULT = 'trunc_normal'. See Layer class for other options.
-            params_dict['biases_initializer'] (str or list of strs, optional): 
-                initializer for the biases in each layer; replicated if a 
-                single element.
-                DEFAULT = 'zeros'. See Layer class for other options.
-            params_dict['log_activations'] (bool, optional): True to use 
-                tf.summary on layer activations
-                DEFAULT = False
+
+            params_dict (dict): contains parameters about details of FFnetwork:
+                params_dict['xstim_n'] (list of ints or None): If not none, the external
+                    stimulus that this network gets input from.
+                params_dict['ffnet_n'] (list of ints or None): If not none, the ffnetwork
+                    that this network gets input from.
+                params_dict['layer_sizes'] (list of ints): list of layer sizes. All
+                    arguments (input size) can be up to a 3-dimensional list.
+                    REQUIRED (NO DEFAULT)
+                params_dict['layer_types'] (list of strs): the type of layer specified
+                    for all layers in the ffnetwork.
+                params_dict['activation_funcs'] (str or list of strs, optional):
+                    pointwise function for each layer; replicated if a single element.
+                    DEFAULT = 'relu'. See Layer class for other options.
+                params_dict['num_inh'] (int or list of ints): denotes number of
+                    inhibitory units in each layer. This specifies the output of
+                    that number of units multiplied by -1
+                    DEFAULT = 0 (and having any single value will be used for all layers)
+                params_dict['pos_constraints'] (bool or list of bools, optional):
+                    constrains all weights to be positive
+                    DEFAULTS = False.
+                params_dict['normalize_weights'] (list of ints): normalization setting for
+                    each layer.
+                params_dict['conv_filter_widths'] (list of ints, None): when set, the width
+                    of the convolutional filter for each layer
+                params_dict['shift_spacing'] (list of ints, None): when set, the shift-spacing
+                    for each layer
+                params_dict['reg_initializer'] (list): a list of dictionaries, one
+                    for each layer. Within the dictionary, reg_type/vals as key-value pairs.
+                    DEFAULT = None
+                params_dict['weights_initializer'] (str or list of strs, optional):
+                    initializer for the weights in each layer; replicated if a single element.
+                    DEFAULT = 'trunc_normal'. See Layer class for other options.
+                params_dict['biases_initializer'] (str or list of strs, optional):
+                    initializer for the biases in each layer; replicated if a single element.
+                    DEFAULT = 'zeros'. See Layer class for other options.
+                params_dict['log_activations'] (bool, optional): True to use, see
+                    tf.summary on layer activations
+                    DEFAULT = False
 
         Raises:
             TypeError: If `scope` is not specified
@@ -221,6 +222,21 @@ class FFNetwork(object):
                     pos_constraint=network_params['pos_constraints'][nn],
                     log_activations=network_params['log_activations']))
 
+            elif self.layer_types[nn] == 'readout':
+
+                self.layers.append(ReadoutLayer(
+                    scope='sep_layer_%i' % nn,
+                    input_dims=layer_sizes[nn],
+                    output_dims=layer_sizes[nn + 1],
+                    activation_func=network_params['activation_funcs'][nn],
+                    normalize_weights=network_params['normalize_weights'][nn],
+                    weights_initializer=network_params['weights_initializers'][nn],
+                    biases_initializer=network_params['biases_initializers'][nn],
+                    reg_initializer=network_params['reg_initializers'][nn],
+                    num_inh=network_params['num_inh'][nn],
+                    pos_constraint=network_params['pos_constraints'][nn],
+                    log_activations=network_params['log_activations']))
+
             elif self.layer_types[nn] == 'add':
 
                 self.layers.append(AddLayer(
@@ -259,11 +275,40 @@ class FFNetwork(object):
                         conv_filter_size[2] = \
                             network_params['conv_filter_widths'][nn]
 
-                self.layers.append(ConvLayer(
+                if self.layer_types[nn] == 'conv':
+                    self.layers.append(ConvLayer(
+                        scope='conv_layer_%i' % nn,
+                        input_dims=layer_sizes[nn],
+                        num_filters=layer_sizes[nn+1],
+                        filter_dims=conv_filter_size,
+                        shift_spacing=network_params['shift_spacing'][nn],
+                        activation_func=network_params['activation_funcs'][nn],
+                        normalize_weights=network_params['normalize_weights'][nn],
+                        weights_initializer=network_params['weights_initializers'][nn],
+                        biases_initializer=network_params['biases_initializers'][nn],
+                        reg_initializer=network_params['reg_initializers'][nn],
+                        num_inh=network_params['num_inh'][nn],
+                        pos_constraint=network_params['pos_constraints'][nn],
+                        log_activations=network_params['log_activations']))
+
+                # Modify output size to take into account shifts
+                if nn < self.num_layers:
+                    layer_sizes[nn+1] = self.layers[nn].output_dims
+
+            elif self.layer_types[nn] == 'conv_xy':
+
+                if network_params['conv_filter_widths'][nn] is not None:
+                    width = network_params['conv_filter_widths'][nn]
+                    conv_filter_size = [layer_sizes[nn][0], width, width]
+                else:
+                    conv_filter_size = layer_sizes[nn]
+
+                self.layers.append(ConvXYLayer(
                     scope='conv_layer_%i' % nn,
                     input_dims=layer_sizes[nn],
-                    num_filters=layer_sizes[nn+1],
+                    num_filters=layer_sizes[nn + 1],
                     filter_dims=conv_filter_size,
+                    xy_out=network_params['xy_out'][nn],
                     shift_spacing=network_params['shift_spacing'][nn],
                     activation_func=network_params['activation_funcs'][nn],
                     normalize_weights=network_params['normalize_weights'][nn],
@@ -276,7 +321,7 @@ class FFNetwork(object):
 
                 # Modify output size to take into account shifts
                 if nn < self.num_layers:
-                    layer_sizes[nn+1] = self.layers[nn].output_dims
+                    layer_sizes[nn + 1] = self.layers[nn].output_dims
 
             elif self.layer_types[nn] == 'convsep':
 
@@ -308,6 +353,31 @@ class FFNetwork(object):
                 # Modify output size to take into account shifts
                 if nn < self.num_layers:
                     layer_sizes[nn+1] = self.layers[nn].output_dims
+
+            elif self.layer_types[nn] == 'conv_readout':
+
+                self.layers.append(ConvReadoutLayer(
+                    scope='conv_readout_layer_%i' % nn,
+                    # this should be the case:
+                    # nlags=network_params['time_expand'][nn],
+                    # but since we don't have temporal side network we'll do this for now:
+                    nlags=None,
+                    input_dims=layer_sizes[nn],
+                    num_filters=layer_sizes[nn + 1],
+                    xy_out=network_params['xy_out'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
+                    normalize_weights=network_params['normalize_weights'][nn],
+                    weights_initializer=network_params['weights_initializers'][nn],
+                    biases_initializer=network_params['biases_initializers'][nn],
+                    reg_initializer=network_params['reg_initializers'][nn],
+                    num_inh=network_params['num_inh'][nn],
+                    pos_constraint=network_params['pos_constraints'][nn],
+                    log_activations=network_params['log_activations']))
+
+                # Modify output size to take into account shifts
+                if nn < self.num_layers:
+                    layer_sizes[nn + 1] = self.layers[nn].output_dims
+
 
             elif self.layer_types[nn] == 'biconv':
 
@@ -425,8 +495,7 @@ class SideNetwork(FFNetwork):
                 DEFAULT = 1
             params_dict['binocular'] (boolean): currently doesn't work
                 DEFAULT = FALSE
-            params_dict['layer_sizes'] (list of ints): see FFNetwork 
-                documentation
+            params_dict['layer_sizes'] (list of ints): see FFNetwork documentation
             params_dict['activation_funcs'] (str or list of strs, optional): 
                 see FFNetwork documentation
             params_dict['weights_initializer'] (str or list of strs, optional): 
@@ -439,38 +508,51 @@ class SideNetwork(FFNetwork):
                 FFNetwork documentation
             params_dict['pos_constraint'] (bool or list of bools, optional): 
                 see FFNetwork documentation
-            params_dict['log_activations'] (bool, optional): see FFNetwork 
-                documentation
+            params_dict['log_activations'] (bool, optional): see FFNetwork documentation
                 
         """
 
+        _conv_types = ['conv', 'convsep', 'biconv']
+        isbinocular = False
         # Determine dimensions of input and pass into regular network initializer
         input_layer_sizes = input_network_params['layer_sizes'][:]
         # Check if entire network is convolutional (then will have spatial input dims)
         all_convolutional = False
         nonconv_inputs = np.zeros(len(input_layer_sizes), dtype=int)
-        if (input_network_params['layer_types'][0] == 'conv') or \
-                (input_network_params['layer_types'][0] == 'biconv'):
+        if input_network_params['layer_types'][0] in _conv_types:
             # then check that all are conv
             all_convolutional = True
             for nn in range(len(input_layer_sizes)):
-                if input_network_params['layer_types'][nn] == 'conv' or \
-                        (input_network_params['layer_types'][0] == 'biconv'):
-                    nonconv_inputs[nn] = input_layer_sizes[nn]*input_network_params['input_dims'][1] *\
+                if input_network_params['layer_types'][nn] in _conv_types:
+                    nonconv_inputs[nn] = input_layer_sizes[nn] * input_network_params['input_dims'][1] *\
                                          input_network_params['input_dims'][2]
+                    if input_network_params['layer_types'][nn] == 'biconv':
+                        isbinocular = True
+                        # then twice as many outputs as filters
+                        nonconv_inputs[nn] *= 2
                 else:
                     all_convolutional = False
                     nonconv_inputs[nn] = input_layer_sizes[nn]
+        else:
+            nonconv_inputs = input_layer_sizes[:]
 
         if all_convolutional:
-            nx_ny = input_network_params['input_dims'][1:3]
-            if input_network_params['layer_types'][0] == 'biconv':
-                nx_ny[0] = int(nx_ny[0]/2)
-                input_layer_sizes[0] = input_layer_sizes[0]*2
-            input_dims = [max(input_layer_sizes)*len(input_layer_sizes), nx_ny[0], nx_ny[1]]
+            nx_ny = input_network_params['input_dims'][1:]
+            if isbinocular:
+                nx_ny[0] = int(nx_ny[0] / 2)
+
+                if input_network_params['layer_types'][0] == 'biconv':
+                    input_layer_sizes[0] = input_layer_sizes[0]*2
+                elif input_network_params['layer_types'][1] == 'biconv':
+                    input_layer_sizes[0] = input_layer_sizes[0] * 2
+                    input_layer_sizes[1] = input_layer_sizes[1] * 2
+            #input_dims = [max(input_layer_sizes)*len(input_layer_sizes), nx_ny[0], nx_ny[1]]
+
+            input_dims = [np.sum(input_layer_sizes), nx_ny[0], nx_ny[1]]
         else:
             nx_ny = [1, 1]
-            input_dims = [len(input_layer_sizes), max(nonconv_inputs), 1]
+            #input_dims = [len(input_layer_sizes), max(nonconv_inputs), 1]
+            input_dims = [np.sum(nonconv_inputs), 1, 1]
 
         super(SideNetwork, self).__init__(
             scope=scope,
@@ -482,6 +564,9 @@ class SideNetwork(FFNetwork):
             self.num_units = input_layer_sizes
         else:
             self.num_units = nonconv_inputs
+
+        # Set up potential side_network regularization (in first layer)
+        self.layers[0].reg.scaffold_setup(self.num_units)
     # END SideNetwork.__init__
 
     def build_graph(self, input_network, params_dict=None):
@@ -489,27 +574,37 @@ class SideNetwork(FFNetwork):
         whole network graph, rather than just a link to its output, so that it 
         can be assembled here"""
 
-        max_units = max(self.num_units)
         num_layers = len(self.num_units)
         with tf.name_scope(self.scope):
 
             # Assemble network-inputs into the first layer
             for input_nn in range(num_layers):
 
-                new_slice = input_network.layers[input_nn].outputs
-                if max_units-self.num_units[input_nn] > 0:
-                    layer_padding = tf.constant([
-                        [0, 0],
-                        [0, (max_units-self.num_units[input_nn])*self.num_space]])
-                    new_slice = tf.pad(new_slice, layer_padding)
+                if (self.num_space == 1) or \
+                        self.num_space == np.prod(input_network.layers[input_nn].output_dims[1:]):
+                    new_slice = tf.reshape(input_network.layers[input_nn].outputs,
+                                           [-1, self.num_space, self.num_units[input_nn]])
+                else:  # spatial positions converted to different filters (binocular)
+                    native_space = np.prod(input_network.layers[input_nn].output_dims[1:])
+                    native_filters = input_network.layers[input_nn].output_dims[0]
+                    tmp = tf.reshape(input_network.layers[input_nn].outputs,
+                                           [-1, 1, native_space, native_filters])
+                    # Reslice into correct spatial arrangement
+                    left_post = tf.slice(tmp, [0, 0, 0, 0], [-1, -1, self.num_space, -1])
+                    right_post = tf.slice(tmp, [0, 0, self.num_space, 0],
+                                          [-1, -1, self.num_space, -1])
+
+                    new_slice = tf.reshape(tf.concat([left_post, right_post], axis=3),
+                                           [-1, self.num_space, self.num_units[input_nn]])
 
                 if input_nn == 0:
-                    inputs_raw = tf.expand_dims(new_slice, 2)
+                    inputs_raw = new_slice
                 else:
-                    inputs_raw = tf.concat([inputs_raw, tf.expand_dims(new_slice, 2)], 2)
+                    inputs_raw = tf.concat([inputs_raw, new_slice], 2)
 
             # Need to put layer dimension with the filters as bottom dimension instead of top
-            inputs = tf.reshape(inputs_raw, [-1, num_layers*max_units*self.num_space])
+            inputs = tf.reshape(inputs_raw, [-1, np.sum(self.num_units)*self.num_space] )
+            #inputs = tf.reshape(inputs_raw, [-1, num_layers*max_units*self.num_space])
 
             # Now standard graph-build (could just call the parent with inputs)
             for layer in range(self.num_layers):

FFnetwork/tffnetwork.py
@@ -88,7 +88,6 @@ class TFFNetwork(FFNetwork):
                     nlags=network_params['time_expand'][nn],
                     input_dims=layer_sizes[nn],
                     output_dims=layer_sizes[nn+1],
-                    partial_fit=network_params['partial_fit'][nn],
                     activation_func=network_params['activation_funcs'][nn],
                     normalize_weights=network_params['normalize_weights'][nn],
                     weights_initializer=network_params['weights_initializers'][nn],
@@ -155,6 +154,38 @@ class TFFNetwork(FFNetwork):
                 if nn < self.num_layers:
                     layer_sizes[nn+1] = self.layers[nn].output_dims
 
+            elif self.layer_types[nn] == 'conv_xy':
+                if network_params['conv_filter_widths'][nn] is None:
+                    conv_filter_size = layer_sizes[nn]
+                else:
+                    conv_filter_size = [
+                        layer_sizes[nn][0],
+                        network_params['conv_filter_widths'][nn], 1]
+                    if layer_sizes[nn][2] > 1:
+                        conv_filter_size[2] = \
+                            network_params['conv_filter_widths'][nn]
+
+                self.layers.append(ConvXYLayer(
+                    scope='conv_xy_layer_%i' % nn,
+                    nlags=network_params['time_expand'][nn],
+                    input_dims=layer_sizes[nn],
+                    num_filters=layer_sizes[nn+1],
+                    filter_dims=conv_filter_size,
+                    xy_out=network_params['xy_out'][nn],
+                    shift_spacing=network_params['shift_spacing'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
+                    normalize_weights=network_params['normalize_weights'][nn],
+                    weights_initializer=network_params['weights_initializers'][nn],
+                    biases_initializer=network_params['biases_initializers'][nn],
+                    reg_initializer=network_params['reg_initializers'][nn],
+                    num_inh=network_params['num_inh'][nn],
+                    pos_constraint=network_params['pos_constraints'][nn],
+                    log_activations=network_params['log_activations']))
+
+                # Modify output size to take into account shifts
+                if nn < self.num_layers:
+                    layer_sizes[nn+1] = self.layers[nn].output_dims
+
             elif self.layer_types[nn] == 'convsep':
                 if network_params['conv_filter_widths'][nn] is None:
                     conv_filter_size = layer_sizes[nn]
@@ -167,7 +198,7 @@ class TFFNetwork(FFNetwork):
                             network_params['conv_filter_widths'][nn]
 
                 self.layers.append(ConvSepLayer(
-                    scope='sepconv_layer_%i' % nn,
+                    scope='convsep_layer_%i' % nn,
                     nlags=network_params['time_expand'][nn],
                     input_dims=layer_sizes[nn],
                     num_filters=layer_sizes[nn+1],
@@ -186,6 +217,59 @@ class TFFNetwork(FFNetwork):
                 if nn < self.num_layers:
                     layer_sizes[nn+1] = self.layers[nn].output_dims
 
+            elif self.layer_types[nn] == 'conv_readout':
+                self.layers.append(ConvReadoutLayer(
+                    scope='conv_readout_layer_%i' % nn,
+                    nlags=network_params['time_expand'][nn],
+                    input_dims=layer_sizes[nn],
+                    num_filters=layer_sizes[nn + 1],
+                    xy_out=network_params['xy_out'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
+                    normalize_weights=network_params['normalize_weights'][nn],
+                    weights_initializer=network_params['weights_initializers'][nn],
+                    biases_initializer=network_params['biases_initializers'][nn],
+                    reg_initializer=network_params['reg_initializers'][nn],
+                    num_inh=network_params['num_inh'][nn],
+                    pos_constraint=network_params['pos_constraints'][nn],
+                    log_activations=network_params['log_activations']))
+
+                # Modify output size to take into account shifts
+                if nn < self.num_layers:
+                    layer_sizes[nn + 1] = self.layers[nn].output_dims
+
+
+            elif self.layer_types[nn] == 'gabor':
+                if network_params['conv_filter_widths'][nn] is None:
+                    conv_filter_size = layer_sizes[nn]
+                else:
+                    conv_filter_size = [
+                        layer_sizes[nn][0],
+                        network_params['conv_filter_widths'][nn], 1]
+                    if layer_sizes[nn][2] > 1:
+                        conv_filter_size[2] = \
+                            network_params['conv_filter_widths'][nn]
+
+                self.layers.append(GaborLayer(
+                    scope='gabor_layer_%i' % nn,
+                    nlags=network_params['time_expand'][nn],
+                    input_dims=layer_sizes[nn],
+                    num_filters=layer_sizes[nn+1],
+                    filter_dims=conv_filter_size,
+                    shift_spacing=network_params['shift_spacing'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
+                    normalize_weights=network_params['normalize_weights'][nn],
+                    weights_initializer=network_params['weights_initializers'][nn],
+                    biases_initializer=network_params['biases_initializers'][nn],
+                    reg_initializer=network_params['reg_initializers'][nn],
+                    num_inh=network_params['num_inh'][nn],
+                    pos_constraint=network_params['pos_constraints'][nn],
+                    log_activations=network_params['log_activations']))
+
+                # Modify output size to take into account shifts
+                if nn < self.num_layers:
+                    layer_sizes[nn+1] = self.layers[nn].output_dims
+
+
             elif self.layer_types[nn] == 'biconv':
                 if network_params['conv_filter_widths'][nn] is None:
                     conv_filter_size = layer_sizes[nn]
@@ -198,7 +282,7 @@ class TFFNetwork(FFNetwork):
                             network_params['conv_filter_widths'][nn]
 
                 self.layers.append(BiConvLayer(
-                    scope='conv_layer_%i' % nn,
+                    scope='buiconv_layer_%i' % nn,
                     nlags=network_params['time_expand'][nn],
                     input_dims=layer_sizes[nn],
                     num_filters=layer_sizes[nn+1],
@@ -222,10 +306,11 @@ class TFFNetwork(FFNetwork):
                     scope='temporal_layer_%i' % nn,
                     nlags=network_params['time_expand'][nn],
                     input_dims=layer_sizes[nn],
-                    output_dims=self.batch_size,
+                    output_dims=layer_sizes[nn],
                     num_filters=layer_sizes[nn + 1],
+                    filter_width=network_params['ca_tent_widths'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
                     batch_size=self.batch_size,
-                    time_spread=self.time_spread,
                     normalize_weights=network_params['normalize_weights'][nn],
                     weights_initializer=network_params['weights_initializers'][nn],
                     biases_initializer=network_params['biases_initializers'][nn],
@@ -246,6 +331,7 @@ class TFFNetwork(FFNetwork):
                     output_dims=layer_sizes[nn],
                     num_filters=layer_sizes[nn + 1],
                     filter_width=network_params['ca_tent_widths'][nn],
+                    activation_func=network_params['activation_funcs'][nn],
                     batch_size=self.batch_size,
                     normalize_weights=network_params['normalize_weights'][nn],
                     weights_initializer=network_params['weights_initializers'][nn],
@@ -329,3 +415,5 @@ def time_expand(inputs, batch_sz, nlags):
         expanded_inputs = tf.reshape(expanded_inputs_tr, (batch_sz, -1))
 
     return expanded_inputs
+
+# define readout_computation here

NDN/NDN.py
@@ -12,6 +12,9 @@ from FFnetwork.ffnetwork import SideNetwork
 from .network import Network
 from NDNutils import concatenate_input_dims
 
+from FFnetwork.tlayer import *
+from FFnetwork.layer import *
+
 
 class NDN(Network):
     """Tensorflow (tf) implementation of Neural Deep Network class
@@ -54,8 +57,8 @@ class NDN(Network):
     """
 
     _allowed_noise_dists = ['gaussian', 'poisson', 'bernoulli']
-    _allowed_layer_types = ['normal', 'conv', 'sep', 'convsep', 'add']
-    # ca_tent # pre0 (stim preprocessing layer?)
+    _allowed_network_types = ['normal', 'side']
+    _allowed_layer_types = ['normal', 'conv', 'sep', 'convsep', 'add', 'biconv', 'spike_history']
 
     def __init__(
             self,
@@ -129,7 +132,6 @@ class NDN(Network):
         # list of output sizes (for Robs placeholders)
         self.output_sizes = [0] * len(ffnet_out)
         self.noise_dist = noise_dist
-        self.poisson_unit_norm = None
         self.tf_seed = tf_seed
 
         self._define_network()
@@ -209,14 +211,13 @@ class NDN(Network):
             ffnet_n = self.ffnet_out[nn]
             self.output_sizes[nn] = \
                 self.networks[ffnet_n].layers[-1].weights.shape[1]
-
     # END NDN._define_network
 
     def _build_graph(
             self,
             learning_alg='adam',
             opt_params=None,
-            variable_list=None):
+            fit_variables=None):
         """NDN._build_graph"""
 
         # Take care of optimize parameters if necessary
@@ -292,7 +293,7 @@ class NDN(Network):
                 self._define_loss()
 
             # Define optimization routine
-            var_list = self._build_fit_variable_list(variable_list)
+            var_list = self._build_fit_variable_list(fit_variables)
 
             with tf.variable_scope('optimizer'):
                 self._define_optimizer(
@@ -329,8 +330,7 @@ class NDN(Network):
             # define cost function
             if self.noise_dist == 'gaussian':
                 with tf.name_scope('gaussian_loss'):
-                    cost.append(
-                        tf.nn.l2_loss(data_out - pred) / nt)
+                    cost.append(tf.nn.l2_loss(data_out - pred) / nt)
                     unit_cost.append(tf.reduce_mean(tf.square(data_out-pred), axis=0))
 
             elif self.noise_dist == 'poisson':
@@ -414,7 +414,7 @@ class NDN(Network):
         return var_list
     # END _generate_variable_list
 
-    def variables_to_fit(self, layers_to_skip=None, fit_biases=False):
+    def fit_variables(self, layers_to_skip=None, fit_biases=False):
         """Generates a list-of-lists-of-lists of correct format to specify all 
         the variables to fit, as an argument for network.train
 
@@ -460,18 +460,47 @@ class NDN(Network):
         return fit_list
         # END NDN.set_fit_variables
 
-    def set_regularization(self, reg_type, reg_val, ffnet_n=0,
-                           layer_target=None):
+
+    def set_partial_fit(self, ffnet_target=0, layer_target=None, value=None):
+        """ Assign partial_fit values
+
+        Args:
+            ffnet_target (int): which network
+            layer_target (int): which layer
+            value (0, 1, or None): partial_fit value to be assigned
+                - 0 ---> fit only temporal part
+                - 1 ---> fit only spatial part
+                - anything else ---> fit everything
+        """
+
+        if value == 0:
+            translation = 'only --temporal--'
+        elif value == 1:
+            translation = 'only --spatial--'
+        else:
+            translation = '--everything--'
+
+        if type(self.networks[ffnet_target].layers[layer_target]) is SepLayer or ConvSepLayer:
+            self.networks[ffnet_target].layers[layer_target].partial_fit = value
+            self.networks[ffnet_target].layers[layer_target].reg.partial_fit = value
+
+            print('....partial_fit value for --net%sL%s-- set to %s, %s will be fit'
+                  % (ffnet_target, layer_target, value, translation))
+        else:
+            raise ValueError('partial fit should be used only with Seplayer families.')
+    # END NDN.set_partial_fit
+
+    def set_regularization(self, reg_type, reg_val, ffnet_target=0, layer_target=None):
         """Add or reassign regularization values
 
         Args:
             reg_type (str): see allowed_reg_types in regularization.py
             reg_val (int): corresponding regularization value
-            ffnet_n (int): which network to assign regularization to 
+            ffnet_target (int): which network to assign regularization to
                 DEFAULT: 0
             layer_target (int or list of ints): specifies which layers the
                 current reg_type/reg_val pair is applied to 
-                DEFAULT: all layers in ffnet_n
+                DEFAULT: all layers in ffnet_target
 
         """
 
@@ -484,7 +513,7 @@ class NDN(Network):
 
         # set regularization at the layer level
         for layer in layer_target:
-            self.networks[ffnet_n].layers[layer].set_regularization(
+            self.networks[ffnet_target].layers[layer].set_regularization(
                 reg_type, reg_val)
     # END set_regularization
 
@@ -546,7 +575,7 @@ class NDN(Network):
             # build iterator object to access elements from dataset
             iterator_tr = dataset.make_one_shot_iterator()
 
-        # Place graph operations on CPU
+        # Potentially place graph operations on CPU
         if not use_gpu:
             #temp_config = tf.ConfigProto(device_count={'GPU': 0})
             with tf.device('/cpu:0'):
@@ -557,33 +586,37 @@ class NDN(Network):
 
         with tf.Session(graph=self.graph, config=self.sess_config) as sess:
 
-            self._restore_params(
-                sess, input_data, output_data, data_filters=data_filters)
+            self._restore_params(sess, input_data, output_data, data_filters=data_filters)
 
-            #cost = self._get_test_cost(
-            #    sess, input_data=input_data, output_data=output_data,
-            #    data_filters=data_filters, test_indxs=data_indxs)
-            num_batches_tr = data_indxs.shape[0] // self.batch_size
-            cost_tr = 0
-            for batch_tr in range(num_batches_tr):
-                batch_indxs_tr = data_indxs[
-                                 batch_tr * self.batch_size:(batch_tr + 1) * self.batch_size]
-                if self.data_pipe_type == 'data_as_var':
-                    feed_dict = {self.indices: batch_indxs_tr}
-                elif self.data_pipe_type == 'feed_dict':
-                    feed_dict = self._get_feed_dict(
-                        input_data=input_data,
-                        output_data=output_data,
-                        data_filters=data_filters,
-                        batch_indxs=batch_indxs_tr)
-                elif self.data_pipe_type == 'iterator':
+            cost_tr = self._get_test_cost(
+                sess=sess,
+                input_data=input_data,
+                output_data=output_data,
+                data_filters=data_filters,
+                test_indxs=data_indxs,
+                test_batch_size=self.batch_size)
+
+            #num_batches_tr = data_indxs.shape[0] // self.batch_size
+            #cost_tr = 0
+            #for batch_tr in range(num_batches_tr):
+            #    batch_indxs_tr = data_indxs[
+            #                     batch_tr * self.batch_size:(batch_tr + 1) * self.batch_size]
+            #    if self.data_pipe_type == 'data_as_var':
+            #        feed_dict = {self.indices: batch_indxs_tr}
+            #    elif self.data_pipe_type == 'feed_dict':
+            #        feed_dict = self._get_feed_dict(
+            #            input_data=input_data,
+            #            output_data=output_data,
+            #            data_filters=data_filters,
+            #            batch_indxs=batch_indxs_tr)
+            #    elif self.data_pipe_type == 'iterator':
                     # get string handle of iterator
-                    iter_handle_tr = sess.run(iterator_tr.string_handle())
-                    feed_dict = {self.iterator_handle: iter_handle_tr}
+            #        iter_handle_tr = sess.run(iterator_tr.string_handle())
+            #        feed_dict = {self.iterator_handle: iter_handle_tr}
 
-                cost_tr += sess.run(self.cost, feed_dict=feed_dict)
+            #    cost_tr += sess.run(self.cost, feed_dict=feed_dict)
 
-            cost_tr /= num_batches_tr
+            #cost_tr /= num_batches_tr
 
         return cost_tr
     # END get_LL
@@ -639,27 +672,25 @@ class NDN(Network):
             data_indxs = np.arange(self.num_examples)
 
         # build datasets if using 'iterator' pipeline
-        if self.data_pipe_type == 'iterator':
-            dataset = self._build_dataset(
-                input_data=input_data,
-                output_data=output_data,
-                data_filters=data_filters,
-                indxs=data_indxs,
-                training_dataset=False,
-                batch_size=self.num_examples)
-            # store info on dataset for buiding data pipeline
-            self.dataset_types = dataset.output_types
-            self.dataset_shapes = dataset.output_shapes
-            # build iterator object to access elements from dataset
-            iterator = dataset.make_one_shot_iterator()
+        #if self.data_pipe_type == 'iterator':
+        #    dataset = self._build_dataset(
+        #        input_data=input_data,
+        #        output_data=output_data,
+        #        data_filters=data_filters,
+        #        indxs=data_indxs,
+        #        training_dataset=False,
+        #        batch_size=self.num_examples)
+        #    # store info on dataset for buiding data pipeline
+        #    self.dataset_types = dataset.output_types
+        #    self.dataset_shapes = dataset.output_shapes
+        #    # build iterator object to access elements from dataset
+        #    iterator = dataset.make_one_shot_iterator()
 
         # Place graph operations on CPU
         if not use_gpu:
-            #temp_config = tf.ConfigProto(device_count={'GPU': 0})
             with tf.device('/cpu:0'):
                 self._build_graph()
         else:
-            #temp_config = tf.ConfigProto(device_count={'GPU': 1})
             self._build_graph()
 
         with tf.Session(graph=self.graph, config=self.sess_config) as sess:
@@ -667,29 +698,13 @@ class NDN(Network):
             self._restore_params(
                 sess, input_data, output_data, data_filters=data_filters)
 
-            if self.data_pipe_type == 'data_as_var':
-                # get the feed_dict for batch_indxs
-                feed_dict = {self.indices: data_indxs}
-                ll_neuron = sess.run(self.unit_cost, feed_dict=feed_dict)
-            elif self.batch_size is None:
-                if self.data_pipe_type == 'feed_dict':
-                    feed_dict = self._get_feed_dict(
-                        input_data=input_data,
-                        output_data=output_data,
-                        data_filters=data_filters,
-                        batch_indxs=data_indxs)
-                elif self.data_pipe_type == 'iterator':
-                    # get string handle of iterator
-                    iter_handle = sess.run(iterator.string_handle())
-                    feed_dict = {self.iterator_handle: iter_handle}
-
-                ll_neuron = sess.run(self.unit_cost, feed_dict=feed_dict)
-            else:
+            if self.batch_size is not None:
                 num_batches_test = data_indxs.shape[0] // self.batch_size
+
                 for batch_test in range(num_batches_test):
                     batch_indxs_test = data_indxs[
-                        batch_test * self.batch_size:
-                        (batch_test + 1) * self.batch_size]
+                                       batch_test * self.batch_size:
+                                       (batch_test + 1) * self.batch_size]
                     if self.data_pipe_type == 'data_as_var':
                         feed_dict = {self.indices: batch_indxs_test}
                     elif self.data_pipe_type == 'feed_dict':
@@ -708,6 +723,20 @@ class NDN(Network):
 
                 ll_neuron = np.divide(unit_cost, num_batches_test)
 
+            else:  # no batch-size given
+                if self.data_pipe_type == 'data_as_var':
+                    feed_dict = {self.indices: data_indxs}
+                elif self.data_pipe_type == 'feed_dict':
+                    feed_dict = self._get_feed_dict(
+                        input_data=input_data,
+                        output_data=output_data,
+                        data_filters=data_filters,
+                        batch_indxs=data_indxs)
+                elif self.data_pipe_type == 'iterator':
+                    feed_dict = {self.iterator_handle: data_indxs}
+
+                ll_neuron = sess.run(self.unit_cost, feed_dict=feed_dict)
+
             if nulladjusted:
                 # note that ll_neuron is negative of the true log-likelihood,
                 # but get_null_ll is not (so + is actually subtraction)
@@ -722,7 +751,7 @@ class NDN(Network):
     # END get_LL_neuron
 
     def generate_prediction(self, input_data, data_indxs=None, use_gpu=False,
-                            ffnet_n=-1, layer=-1):
+                            ffnet_target=-1, layer_target=-1):
         """Get cost for each output neuron without regularization terms
 
         Args:
@@ -730,17 +759,17 @@ class NDN(Network):
             data_indxs (numpy array, optional): indexes of data to use in
                 calculating forward pass; if not supplied, all data is used
             use_gpu (True or False): Obvious
-            ffnet_n (int, optional): index into `network_list` that specifies 
+            ffnet_target (int, optional): index into `network_list` that specifies
                 which FFNetwork to generate the prediction from
-            layer (int, optional): index into layers of network_list[ffnet_n]
+            layer_target (int, optional): index into layers of network_list[ffnet_target]
                 that specifies which layer to generate prediction from
 
         Returns:
-            numpy array: pred values from network_list[ffnet_n].layers[layer]
+            numpy array: pred values from network_list[ffnet_target].layers[layer]
                 
         Raises:
             ValueError: If `layer` index is larger than number of layers in
-                network_list[ffnet_n]                
+                network_list[ffnet_target]
 
         """
 
@@ -754,35 +783,31 @@ class NDN(Network):
                     'Input data dims must match across input_data.')
         if data_indxs is None:
             data_indxs = np.arange(self.num_examples)
-        if layer >= len(self.networks[ffnet_n].layers):
+        if layer_target >= len(self.networks[ffnet_target].layers):
                 ValueError('This layer does not exist.')
         if data_indxs is None:
             data_indxs = np.arange(self.num_examples)
 
-        # Generate fake_output data and take care of data-filtering, in case
-        # necessary
+        # change data_pipe_type to feed_dict
+        #original_pipe_type = deepcopy(self.data_pipe_type)
+        #self.data_pipe_type = 'data_as_var'
+
+        # Generate fake_output data and take care of data-filtering, in case necessary
         self.filter_data = False
         num_outputs = len(self.ffnet_out)
         output_data = [None] * num_outputs
         for nn in range(num_outputs):
             output_data[nn] = np.zeros(
-                [self.num_examples,
-                 self.networks[ffnet_n].layers[-1].weights.shape[1]],
+                [self.num_examples, self.networks[self.ffnet_out[nn]].layers[-1].weights.shape[1]],
                 dtype='float32')
 
-        # build datasets if using 'iterator' pipeline
-        if self.data_pipe_type == 'iterator':
-            dataset = self._build_dataset(
-                input_data=input_data,
-                output_data=output_data,
-                indxs=data_indxs,
-                training_dataset=False,
-                batch_size=self.num_examples)
-            # store info on dataset for buiding data pipeline
-            self.dataset_types = dataset.output_types
-            self.dataset_shapes = dataset.output_shapes
-            # build iterator object to access elements from dataset
-            iterator = dataset.make_one_shot_iterator()
+        if (self.batch_size is None) or (self.batch_size > data_indxs.shape[0]):
+            batch_size_save = self.batch_size
+            self.batch_size = data_indxs.shape[0]
+        else:
+            batch_size_save = None
+
+        num_batches_test = data_indxs.shape[0] // self.batch_size
 
         # Place graph operations on CPU
         if not use_gpu:
@@ -797,20 +822,31 @@ class NDN(Network):
 
             self._restore_params(sess, input_data, output_data)
 
-            if self.data_pipe_type == 'data_as_var':
-                feed_dict = {self.indices: data_indxs}
-            elif self.data_pipe_type == 'feed_dict':
-                feed_dict = self._get_feed_dict(
-                    input_data=input_data,
-                    batch_indxs=data_indxs)
-            elif self.data_pipe_type == 'iterator':
-                # get string handle of iterator
-                iter_handle = sess.run(iterator.string_handle())
-                feed_dict = {self.iterator_handle: iter_handle}
+            for batch_test in range(num_batches_test):
 
-            pred = sess.run(
-                self.networks[ffnet_n].layers[layer].outputs,
-                feed_dict=feed_dict)
+                batch_indxs_test = data_indxs[batch_test * self.batch_size:(batch_test + 1) * self.batch_size]
+                feed_dict = {self.indices: batch_indxs_test}
+
+                if batch_test == 0:
+                    pred = sess.run(
+                        self.networks[ffnet_target].layers[layer_target].outputs,
+                        feed_dict=feed_dict)
+                else:
+                    pred = np.concatenate( (pred, sess.run(
+                        self.networks[ffnet_target].layers[layer_target].outputs,
+                        feed_dict=feed_dict)), axis=0)
+
+            # Add last fraction (if batches did not include all data
+            if pred.shape[0] < data_indxs.shape[0]:
+                batch_indxs_test = data_indxs[num_batches_test * self.batch_size:data_indxs.shape[0]]
+                feed_dict = {self.indices: batch_indxs_test}
+                pred = np.concatenate((pred, sess.run(
+                    self.networks[ffnet_target].layers[layer_target].outputs, feed_dict=feed_dict)), axis=0)
+
+        # change the data_pipe_type to original
+        # self.data_pipe_type = original_pipe_type
+        if batch_size_save is not None:
+            self.batch_size = batch_size_save
 
         return pred
     # END generate_prediction
@@ -819,6 +855,9 @@ class NDN(Network):
         """Return reg penalties in a dictionary"""
 
         reg_dict = {}
+        if self.graph is None:
+            self._build_graph()
+
         with tf.Session(graph=self.graph, config=self.sess_config) as sess:
 
             # initialize all parameters randomly
@@ -876,8 +915,7 @@ class NDN(Network):
         sio.savemat(filename, matdata)
 
     def initialize_output_layer_bias(self, robs):
-        """Sets biases in output layer(w) to explain mean firing rate, using 
-        Robs"""
+        """Sets biases in output layer(w) to explain mean firing rate, using Robs"""
 
         if robs is not list:
             robs = [robs]

code.py
@@ -129,21 +129,21 @@ d2x = 0.0005
 l1 = 0.000001
 
 HSMparams1 = NDNutils.ffnetwork_params(
-    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC1], ei_layers=[0,int(hls/2)],layers_to_normalize=[0],
-    reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
+    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC1], ei_layers=[0,int(hls/2)], normalization=[0], 
+    layer_types=['normal','normal','normal'], reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
 HSM1 = NDN.NDN( HSMparams1, noise_dist='gaussian' )
 HSMparams2 = NDNutils.ffnetwork_params(
-    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC2], ei_layers=[0,int(hls/2)],layers_to_normalize=[0],
-    reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
+    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC2], ei_layers=[0,int(hls/2)], normalization=[0], 
+    layer_types=['normal','normal','normal'], reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
 HSM2 = NDN.NDN( HSMparams2, noise_dist='gaussian' )
 HSMparams3 = NDNutils.ffnetwork_params(
-    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC3], ei_layers=[0,int(hls/2)],layers_to_normalize=[0],
-    reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
+    input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC3], ei_layers=[0,int(hls/2)], normalization=[0], 
+    layer_types=['normal','normal','normal'], reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
 HSM3 = NDN.NDN( HSMparams3, noise_dist='gaussian' )
 
-iter = HSM1.train( input_data=Cstim1, output_data=Crobs1, train_indxs=np.array(range(NT1)))
-iter = HSM2.train( input_data=Cstim2, output_data=Crobs2, train_indxs=np.array(range(NT2)))
-iter = HSM3.train( input_data=Cstim3, output_data=Crobs3, train_indxs=np.array(range(NT3)))
+iter = HSM1.train( input_data=Cstim1, output_data=Crobs1, train_indxs=np.array(range(NT1)), learning_alg="lbfgs")
+iter = HSM2.train( input_data=Cstim2, output_data=Crobs2, train_indxs=np.array(range(NT2)), learning_alg="lbfgs")
+iter = HSM3.train( input_data=Cstim3, output_data=Crobs3, train_indxs=np.array(range(NT3)), learning_alg="lbfgs")
 
 pred1 = HSM1.generate_prediction(input_data=Cstim1)[range(NT1, NT1+50)]
 pred2 = HSM2.generate_prediction(input_data=Cstim2)[range(NT2, NT2+50)]
@@ -151,20 +151,27 @@ pred3 = HSM3.generate_prediction(input_data=Cstim3)[range(NT3, NT3+50)]
 
 
 HSMparamsC = NDNutils.ffnetwork_params(
-        input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC], ei_layers=[0,int(hls/2)],layers_to_normalize=[0],
-        reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
+        input_dims=[1,NX,NX], layer_sizes=[hls,2*hls,NC], ei_layers=[0,int(hls/2)], normalization=[0],
+        layer_types=['normal','normal','normal'], reg_list={'d2x':[d2x,None,None],'l1':[l1,None,None],'max':[None,None,100]})
 HSMcomb = NDN.NDN( HSMparamsC, noise_dist='gaussian')
     
-iter = HSMcomb.train( input_data=Cstim, output_data=Crobs, train_indxs=np.array(Ui), data_filters=Cval_resps)
+iter = HSMcomb.train( 
+        input_data=Cstim, output_data=Crobs, train_indxs=np.array(Ui), data_filters=Cval_resps, learning_alg="lbfgs")
+
+# JM EDIT - otherwise fails
+HSM1.batch_size=50;
+HSM2.batch_size=50;
+HSM3.batch_size=50;
 
 # Evaluate models on different cross-val sets
-LLs1 = HSM1.eval_models( input_data=Cstim1, output_data=Crobs1, data_indxs=range(NT1, NT1+50) )
-LLs2 = HSM2.eval_models( input_data=Cstim2, output_data=Crobs2, data_indxs=range(NT2, NT2+50) )
-LLs3 = HSM3.eval_models( input_data=Cstim3, output_data=Crobs3, data_indxs=range(NT3, NT3+50) )
+LLs1 = HSM1.eval_models( input_data=Cstim1, output_data=Crobs1, data_indxs=np.array(range(NT1, NT1+50)) )
+LLs2 = HSM2.eval_models( input_data=Cstim2, output_data=Crobs2, data_indxs=np.array(range(NT2, NT2+50)) )
+LLs3 = HSM3.eval_models( input_data=Cstim3, output_data=Crobs3, data_indxs=np.array(range(NT3, NT3+50)) )
 
-LLsC1 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs= range(NT, NT+50), data_filters=Cval_resps )
-LLsC2 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs= range(NT+50, NT+100), data_filters=Cval_resps )
-LLsC3 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs= range(NT+100, NT+150), data_filters=Cval_resps )
+HSMcomb.batch_size=50;
+LLsC1 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs=np.array(range(NT, NT+50)), data_filters=Cval_resps )
+LLsC2 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs=np.array(range(NT+50, NT+100)), data_filters=Cval_resps )
+LLsC3 = HSMcomb.eval_models( input_data=Cstim, output_data=Crobs, data_indxs=np.array(range(NT+100, NT+150)), data_filters=Cval_resps )
 
 pred_comb1 = HSMcomb.generate_prediction(input_data=Cstim)[range(NT, NT+50)][:,range(NC1)]
 pred_comb2 = HSMcomb.generate_prediction(input_data=Cstim)[range(NT+50, NT+100)][:,range(NC1,NC1+NC2)]